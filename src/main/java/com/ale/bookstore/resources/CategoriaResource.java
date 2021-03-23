cf54ba4cc74c8ad03317f5d266dbf4db5982a28a
alexandre251387 <ajfs2513@gmail.com>
2021-03-23 13:47:56 -0300

    criação do findAll para categorias

--- src/main/java/com/ale/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/ale/bookstore/resources/CategoriaResource.java
@@ -1,5 +1,8 @@
 package com.ale.bookstore.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ale.bookstore.domain.Categoria;
+import com.ale.bookstore.dtos.CategoriaDTO;
 import com.ale.bookstore.service.CategoriaService;
 
 @RestController
@@ -21,5 +25,12 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> findById(@PathVariable Integer id){
 		Categoria obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll(){
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDtos = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDtos);
 	}
 }
